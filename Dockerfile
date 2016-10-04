76adc8173ddb2e93c38d49283d6594060749332a
mb1a15 <mb4e10@soton.ac.uk>
2016-10-04 14:45:17 +0100

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:16.04
+
+MAINTAINER Marijan Beg <m.beg@soton.ac.uk>
+
+RUN apt-get update -y
+RUN apt-get install -y git python3-pip curl
+RUN python3 -m pip install --upgrade pip pytest-cov codecov numpy
+
+WORKDIR /usr/local/
+
+RUN git clone https://github.com/joommf/joommfutil.git
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.rst") as f:
+    readme = f.read()
+
+setup(
+    name="joommfutil",
+    version="0.5.2",
+    description="A JOOMMF utilities package.",
+    long_description=readme,
+    author="Computational Modelling Group",
+    author_email="fangohr@soton.ac.uk",
+    url="https://github.com/joommf/joommfutil",
+    download_url="https://github.com/joommf/joommfutil/tarball/0.5.2",
+    packages=["joommfutil",
+              "joommfutil.typesystem",
+              "joommfutil.tests"],
+    install_requires=["numpy"],
+    classifiers=["License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python :: 3"]
+)
