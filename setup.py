4ca707a73fc33d2c6494e3210d8fdf2587f9de1f
mb1a15 <mb4e10@soton.ac.uk>
2016-10-18 12:05:13 +0100

    Make quotes consistent.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
-with open("README.rst") as f:
+with open('README.rst') as f:
     readme = f.read()
 
 setup(
-    name="joommfutil",
-    version="0.5.4.2",
-    description="A JOOMMF utilities package.",
+    name='joommfutil',
+    version='0.5.4.2',
+    description='A JOOMMF utilities package.',
     long_description=readme,
-    url="https://github.com/joommf/joommfutil",
-    author="Computational Modelling Group",
-    author_email="fangohr@soton.ac.uk",
+    url='https://github.com/joommf/joommfutil',
+    author='Computational Modelling Group',
+    author_email='fangohr@soton.ac.uk',
     packages=find_packages(),
-    install_requires=["numpy"],
+    install_requires=['numpy'],
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: BSD License',
                  'Topic :: Scientific/Engineering :: Physics',
