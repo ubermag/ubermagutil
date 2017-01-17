a25efb7e973993483731fee4bf939671e395d440
mb1a15 <m.beg@soton.ac.uk>
2017-01-17 16:27:44 +0000

    Remove version.

--- joommfutil/__init__.py
+++ joommfutil/__init__.py
@@ -1,8 +1,3 @@
-import pkg_resources as pr
-
-__version__ = pr.get_distribution("joommfutil").version
-
-
 def test():
     import pytest  # pragma: no cover
     pytest.main(["-v", "--pyargs", "joommfutil"])  # pragma: no cover
--- joommfutil/tests/test_version.py
+++
@@ -1,5 +0,0 @@
-import joommfutil as ju
-
-
-def test_version():
-    assert isinstance(ju.__version__, str)
