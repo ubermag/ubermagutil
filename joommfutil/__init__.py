ce38082aab0a07d77dd414dfb72910cdd9932322
mb1a15 <m.beg@soton.ac.uk>
2017-01-17 16:18:48 +0000

    Read __version__ from setup.py.

--- joommfutil/__init__.py
+++ joommfutil/__init__.py
@@ -1,3 +1,8 @@
+import pkg_resources as pr
+
+__version__ = pr.get_distribution("micromagneticmodel").version
+
+
 def test():
     import pytest  # pragma: no cover
     pytest.main(["-v", "--pyargs", "joommfutil"])  # pragma: no cover
---
+++ joommfutil/tests/test_version.py
@@ -0,0 +1,5 @@
+import joommfutil as ju
+
+
+def test_version():
+    assert isinstance(ju.__version__, str)
