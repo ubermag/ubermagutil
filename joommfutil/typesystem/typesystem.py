6116dc3ec06c8a8af0fe92437be5231a9023092c
mb1a15 <m.beg@soton.ac.uk>
2017-05-03 14:40:02 +0100

    Add an example in doctest for testing.

--- joommfutil/typesystem/typesystem.py
+++ joommfutil/typesystem/typesystem.py
@@ -3,6 +3,19 @@ from .constantdescriptors import ConstantDescriptor
 
 
 def typesystem(**kwargs):
+    """Decorator for imposing typesystem.
+
+    Examples
+    --------
+    Simple class decorating.
+
+    >>> import joommfutil.typesystem as ts
+    >>> @ts.typesystem(a=ts.UnsignedReal)
+    ... class A:
+    ...     def __init__(self, a):
+    ...         self.a = a
+ 
+    """
     def decorate(cls):
         for key, value in kwargs.items():
             if isinstance(value, (Descriptor, ConstantDescriptor)):
