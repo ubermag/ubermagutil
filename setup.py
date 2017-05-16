e81c118e55286ce426620a8618d3f9969eda9f57
mb1a15 <m.beg@soton.ac.uk>
2017-05-16 16:39:00 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setuptools.setup(
     name='joommfutil',
-    version='0.7.2',
+    version='0.7.3',
     description='A JOOMMF utilities package.',
     long_description=readme,
     url='https://joommf.github.io',
