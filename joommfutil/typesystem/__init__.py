8ab74cc1d2d29a85f1e69389ebd4109845a33fab
Marijan Beg <marijan.beg@xfel.eu>
2018-07-05 15:13:49 +0200

    Add docstring.

--- joommfutil/typesystem/__init__.py
+++ joommfutil/typesystem/__init__.py
@@ -1,2 +1,3 @@
-from .descriptors import Typed, Scalar, Vector, Name, InSet, Subset
+from .descriptors import Descriptor, Typed, Scalar, Vector, \
+    Name, InSet, Subset
 from .typesystem import typesystem
