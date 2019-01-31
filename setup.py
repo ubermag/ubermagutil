6a469e69161d24a24da45f76177e8cd5e73f2271
Marijan Beg <marijan.beg@xfel.eu>
2019-01-31 14:55:40 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='joommfutil',
-    version='0.8.3',
+    version='0.8.4',
     description='Python utilities package used across all JOOMMF packages.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://joommf.github.io',
-    author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
+    author='Marijan Beg, Sergii Mamedov, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
     include_package_data = True,
