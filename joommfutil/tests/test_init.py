eda1f34191cc2b9ec1801913bbf7739623992f00
Marijan Beg <marijan.beg@xfel.eu>
2019-01-31 14:45:19 +0100

    PEP8

--- joommfutil/tests/test_init.py
+++ joommfutil/tests/test_init.py
@@ -1,10 +1,11 @@
 import joommfutil as ju
 
+
 def test_version():
     assert isinstance(ju.__version__, str)
     assert '.' in ju.__version__
 
+
 def test_dependencies():
     assert isinstance(ju.__dependencies__, list)
     assert len(ju.__dependencies__) > 0
-    
