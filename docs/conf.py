f8181e2006711a1a3568f4112ef5c40162347cbd
Marijan Beg <marijan.beg@xfel.eu>
2019-02-01 14:50:11 +0100

    Update docs.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Marijan Beg, Sergii Mamedov, Ryan A. Pepper, Thomas Kluyver, and Hans 
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.8.3'
+release = ''
 
 
 # -- General configuration ---------------------------------------------------
