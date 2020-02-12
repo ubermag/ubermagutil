a4778260bb9e588b89046042f60608a7d3d7f60e
Marijan Beg <m.beg@soton.ac.uk>
2020-02-12 16:31:53 +0000

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='ubermagutil',
-    version='0.2.1',
+    version='0.2.2',
     description=('Python utilities package used across '
                  'most of Ubermag packages.'),
     long_description=long_description,
