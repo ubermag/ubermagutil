e0fd28fc7a8d3de36bf71fb065145cc2b0301fa9
marijanbeg <m.beg@soton.ac.uk>
2021-02-10 14:11:33 +0000

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='ubermagutil',
-    version='0.4',
+    version='0.4.1',
     description='Utility package used across Ubermag.',
     author='Marijan Beg, Martin Lang, and Hans Fangohr',
     long_description=long_description,
