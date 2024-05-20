955e44cf0379ad09fbe17bad488086bd29423e39
Martin Lang <martin.lang@mpsd.mpg.de>
2024-05-20 09:39:12 +0200

    Redundant import aliases in __init__ to avoid marking imports as unused
    Suggested here: https://docs.astral.sh/ruff/rules/unused-import/

--- ubermagutil/units/__init__.py
+++ ubermagutil/units/__init__.py
@@ -1,3 +1,6 @@
 """SI multiplier utility."""
 
-from .units import rsi_prefixes, si_max_multiplier, si_multiplier, si_prefixes
+from .units import rsi_prefixes as rsi_prefixes
+from .units import si_max_multiplier as si_max_multiplier
+from .units import si_multiplier as si_multiplier
+from .units import si_prefixes as si_prefixes
